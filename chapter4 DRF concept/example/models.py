572774fce3dd34989edaec996d71bd994b3730ef
KimSound424 <ykj1512@gmail.com>
2023-07-13 15:32:36 +0900

    models.py

--- chapter4 DRF concept/example/models.py
+++ chapter4 DRF concept/example/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    bid = models.IntegerField(primary_key=True) #book id
+    title = models.CharField(max_length=50) #book title
+    author = models.CharField(max_length=50) #book author
+    category = models.CharField(max_length=50) #book category
+    pages = models.IntegerField() #book pages
+    price = models.IntegerField() #book price
+    published_date = models.DateField() #book published date
+    description = models.TextField() #book description
