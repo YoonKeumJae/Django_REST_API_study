15565308d864e33529a09072e53eaf68cb21cd95
KimSound424 <ykj1512@gmail.com>
2023-07-13 18:01:28 +0900

    Book information API Build

--- chapter4 DRF concept/example/urls.py
+++ chapter4 DRF concept/example/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import HelloAPI
+from .views import HelloAPI, booksAPI, bookAPI
 
 urlpatterns = [
-    path("hello/", HelloAPI)
+    path("hello/", HelloAPI),
+    path("books/", booksAPI),
+    path("book/<int:bid>/", bookAPI)
 ]
